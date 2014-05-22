15edb2dde3b7b5ab1eb8547b54d5e1f0e9443869
Marnen Laibow-Koser <marnen@marnen.org>
2014-05-22 12:21:03 -0400

    Use shorter syntax. [#1]

--- spec/positron_spec.rb
+++ spec/positron_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe Positron do
   describe '.included' do
     it "creates a new ActiveRecord class, accessible through the host class's .db method" do
-      host_class = Class.new
-      host_class.class_eval { include Positron }
+      host_class = Class.new { include Positron }
 
       db_class = host_class.send(:db)
       db_class.should be_a_kind_of Class
