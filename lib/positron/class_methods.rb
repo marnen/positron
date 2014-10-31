e7dc158f20416f38af7c0dfd0d834368dab9bd3b
Marnen Laibow-Koser <marnen@marnen.org>
2014-10-31 12:29:02 -0400

    Set table name from host class. [#6]

--- lib/positron/class_methods.rb
+++ lib/positron/class_methods.rb
@@ -2,15 +2,8 @@ module Positron::ClassMethods
   private
 
   def db(&block)
-    unless @_db
-      @_db = Positron::DbClass.new
-      @_db.instance_variable_set :@host_class, self
-      @_db.class_eval do
-        def self.table_name
-          @host_class.name.to_s.underscore.pluralize
-        end
-      end
-    end
+    host_class = self
+    @_db = Positron::DbClass.new host_class unless @_db
     @_db.tap do
       if block_given?
         @_db.class_eval &block
--- spec/positron/db_class_spec.rb
+++ spec/positron/db_class_spec.rb
@@ -3,10 +3,18 @@ require 'spec_helper'
 module Positron
   describe DbClass do
     describe '.new' do
-      it 'should create an ActiveRecord subclass' do
-        db_class = DbClass.new
+      it 'creates an ActiveRecord subclass' do
+        db_class = DbClass.new Class.new
         db_class.should be_a_kind_of Class
         db_class.new.should be_a_kind_of ActiveRecord::Base
+      end
+
+      it 'sets the table name from the host class argument' do
+        host_class = Class.new
+        class_name = Faker::Lorem.words(2).map(&:titleize).join
+        host_class.stub name: class_name
+
+        DbClass.new(host_class).table_name.should == class_name.underscore.pluralize
       end
     end
   end
