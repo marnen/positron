73654cffb14d7972d068a420599fb5e6f737392e
Marnen Laibow-Koser <marnen@marnen.org>
2014-10-31 12:55:45 -0400

    Use ActiveSupport::Concern. [#5]

--- lib/positron.rb
+++ lib/positron.rb
@@ -1,7 +1,5 @@
 Dir.glob(File.join File.dirname(__FILE__), 'positron/**/*.rb') {|file| require file }
 
 module Positron
-  def self.included(base)
-    base.extend Positron::ClassMethods
-  end
+  extend ActiveSupport::Concern
 end
